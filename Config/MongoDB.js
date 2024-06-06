c1e8afa4bd9a76ca1d3f675ac1b186af158e1868
Dhruv Dixit <dhruvmes.8555@gmail.com>
2024-06-07 02:53:20 +0530

    MongoDB

--- Config/MongoDB.js
+++ Config/MongoDB.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
+const MongoDBURI = "mongodb+srv://dhruvmes8555:Uw6SUWFzEEcOAe0e@cluster0.l2potue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/TO-DO_app"
 
 const new_connection = mongoose
-    .createConnection("mongodb+srv://dhruvmes8555:Uw6SUWFzEEcOAe0e@cluster0.l2potue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/TO-DO_app")
-    .on('open', () => console.log("New connections 'TO-DO_app' DB is Connected."))
-    .on('error', (err) => console.log('Connection error! => ', err));
+    .connect(process.env.MongoDBURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true, // Ensure SSL is enabled
+        tlsInsecure: true 
+    })
+    .then(() => console.log("New connections 'TO-DO_app' DB is Connected."))
+    .catch((err) => console.log('Connection error! => ', err));
 
 module.exports = new_connection;
