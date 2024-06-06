5464f6a79109775a599ecb3c2975b02b70916e2e
Dhruv Dixit <dhruvmes.8555@gmail.com>
2024-06-07 02:29:05 +0530

    Switch to MongoDB Atlas

--- Config/MongoDB.js
+++ Config/MongoDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const new_connection = mongoose
-    .createConnection("mongodb://127.0.0.1:27017/TO-DO_app")
+    .createConnection("mongodb+srv://dhruvmes8555:Uw6SUWFzEEcOAe0e@cluster0.l2potue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/TO-DO_app")
     .on('open', () => console.log("New connections 'TO-DO_app' DB is Connected."))
     .on('error', (err) => console.log('Connection error! => ', err));
 
