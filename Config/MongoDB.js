629cd3d403c98167b779c705e92789642cfd645f
Dhruv Dixit <dhruvmes.8555@gmail.com>
2024-06-07 16:10:54 +0530

    Mongo

--- Config/MongoDB.js
+++ Config/MongoDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const localhostServer = "mongodb://127.0.0.1:27017/To-Do_App";
 
 const new_connection = mongoose
-    .connect(process.env.MongoDBURI || localhostServer, {
+    .connect(process.env.MongoDBURI, {
         // useNewUrlParser: true,
         // useUnifiedTopology: true,
         ssl: true,
