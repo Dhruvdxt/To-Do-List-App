ffdb977d13105f0908ba0a046fda55dcd949fe91
Dhruv Dixit <dhruvmes.8555@gmail.com>
2024-06-07 15:35:39 +0530

    Connected to MongoDB Atlas

--- Config/MongoDB.js
+++ Config/MongoDB.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-const MongoDBURI = "mongodb+srv://dhruvmes8555:Uw6SUWFzEEcOAe0e@cluster0.l2potue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/TO-DO_app"
 
 const new_connection = mongoose
     .connect(process.env.MongoDBURI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        ssl: true, // Ensure SSL is enabled
+        ssl: true,
         tlsInsecure: true 
     })
     .then(() => console.log("New connections 'TO-DO_app' DB is Connected."))
